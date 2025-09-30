87ce208fbfe1f48610f2622c5e97fa9a2df5aa37
Joshua Akinyemi <joshuacakinyemi@gmail.com>
2025-09-30 11:01:31 -0400

    solve the challenges.

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ Complete the function so that it returns the following:
 Below 40 → "Wear a heavy coat, scarf, and boots."
 */
 
+const randomNumber = Math.floor(Math.random() * 101)
+
 const outfitPicker = (temp) => {
-  // ✍️ Write your code here
+  if (temp >= 80) {
+    return "Wear shorts and a t-shirt.";
+  } else if (temp >= 60 && temp <= 79) {
+    return "Wear jeans and a light jacket.";
+  } else if (temp >= 40 && temp <= 59) {
+    return "Wear a sweater and pants.";
+  } else {
+    return "Wear a heavy coat, scarf, and boots.";
+  }
 };
 
+
 //Test your function below
+console.log("Today's tempersure is " + randomNumber + " degrees, " + outfitPicker(randomNumber))
